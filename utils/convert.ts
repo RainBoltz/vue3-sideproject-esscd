a98f81a9a8cc13b279392a39a0df9e4e1ee0d754
Philip Wang <philip27020012@gmail.com>
2023-05-25 15:52:58 +0800

    chore: remove unneeds

--- utils/convert.ts
+++
@@ -1,7 +0,0 @@
-
-export const typeToIconCodePoint = (type: string | null) => {
-    switch (type) {
-        case 'AP': return '&#xe875';
-        default: return '&#xe30b';
-    }
-}
--- utils/screen.ts
+++
@@ -1,4 +0,0 @@
-
-export const disablePageScrolling = () => {
-    document.documentElement.style.overflow = 'hidden';
-}
